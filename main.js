3e625deff0bd4b92e2315b7c30ee0a977a675fab
Christine Rowland <fordo29@gmail.com>
2021-10-30 00:01:30 -0600

    Submit coding for button

--- main.js
+++ main.js
@@ -1,13 +1,63 @@
+var sides = [
+  'Miso Glazed Carrots',
+  'Coleslaw',
+  'Garden Salad',
+  'Crispy Potatoes',
+  'Sweet Potato Tots',
+  'Coconut Rice',
+  'Caeser Salad',
+  'Shrimp Summer Rolls',
+  'Garlic Butter Mushrooms',
+  'Hush Puppies'
+]
+
+var mains = [
+  'Spaghetti and Meatballs',
+  'Pineapple Chicken',
+  'Shakshuka',
+  'Thai Yellow Curry',
+  'Bibimbap',
+  'Chicken Parmesean',
+  'Butternut Squash Soup',
+  'BBQ Chicken Burgers',
+  'Ramen',
+  'Empanadas',
+  'Chicken Fried Rice',
+  'Sheet Pan Fajitas',
+  'Margarita Pizza'
+]
+
+var desserts = [
+  'Apple Pie',
+  'Lemon Meringue Pie',
+  'Black Forest Cake',
+  'Banana Bread',
+  'Peach Cobbler',
+  'Cheesecake',
+  'Funfetti Cake',
+  'Baklava',
+  'Flan',
+  'Macarons',
+  'Macaroons',
+  'Chocolate Cupcakes',
+  'Pavlova',
+  'Pumpkin Pie',
+  'Key Lime Pie',
+  'Tart Tatin',
+  'Croissants',
+  'Eclairs',
+]
 // Query selectors
-var sideRadioButton = document.querySelector("#side");
-var mainRadioButton = document.querySelector("#main");
-var dessertRadioButton = document.querySelector("#dessert");
-var mealRadioButton = document.querySelector("#meal");
+var sideRadioButton = document.getElementById("side");
+var mainRadioButton = document.getElementById("main");
+var dessertRadioButton = document.getElementById("dessert");
+var mealRadioButton = document.getElementById("meal");
 var letsCookButton = document.querySelector(".lets-cook");
 
 //views
 var cookpotView = document.querySelector(".cookpot");
 var recipeView = document.querySelector(".showFood");
+var singleFoodView = document.querySelector(".show-single-food");
 
 
 //event listeners
@@ -19,79 +69,39 @@ function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
     }
 
-function displayFood() {
-  showFood.innerHTML = ``;
+function randomizeFood() {
   if (sideRadioButton.checked){
-    sides[getRandomIndex(sides)]`
-    }
-
+    return sides[getRandomIndex(sides)];
+  } else if (mainRadioButton.checked) {
+    return mains[getRandomIndex(mains)];
+  } else if (dessertRadioButton.checked) {
+    return desserts[getRandomIndex(desserts)];
+  // } else if (mealRadioButton) {
+  //
+  // }
   }
-
-console.log(recipeView.style);
-//   if(sideRadioButton === true) {
-//     for(var i = 0; i < sides.length; i++) {
-//
-//     }
-//   }
-//   showFoodView();
-// }
-
-
-// function showFoodView() {
-//   cookpotView.classList.add('hidden');
-//   recipeView.classList.remove('hidden');
-// }
-//
-// function showCookPot() {
-//   cookpotView.classList.remove('hidden');
-//   recipeView.classList.add('hidden');
 }
-var sides = [
-'Miso Glazed Carrots',
-'Coleslaw',
-'Garden Salad',
-'Crispy Potatoes',
-'Sweet Potato Tots',
-'Coconut Rice',
-'Caeser Salad',
-'Shrimp Summer Rolls',
-'Garlic Butter Mushrooms',
-'Hush Puppies'
-]
 
-var mains = [
-'Spaghetti and Meatballs',
-'Pineapple Chicken',
-'Shakshuka',
-'Thai Yellow Curry',
-'Bibimbap',
-'Chicken Parmesean',
-'Butternut Squash Soup',
-'BBQ Chicken Burgers',
-'Ramen',
-'Empanadas',
-'Chicken Fried Rice',
-'Sheet Pan Fajitas',
-'Margarita Pizza'
-]
+function displayFood() {
+  // recipeView.innerText = ``;
+  singleFoodView.innerText = randomizeFood();
+  showFoodView();
+}
 
-var desserts = [
-'Apple Pie',
-'Lemon Meringue Pie',
-'Black Forest Cake',
-'Banana Bread',
-'Peach Cobbler',
-'Cheesecake',
-'Funfetti Cake',
-'Baklava',
-'Flan',
-'Macarons',
-'Macaroons',
-'Chocolate Cupcakes',
-'Pavlova',
-'Pumpkin Pie',
-'Key Lime Pie',
-'Tart Tatin',
-'Croissants',
-'Eclairs',
-]
+
+
+
+console.log(displayFood());
+
+
+
+function showFoodView() {
+  cookpotView.classList.add('hidden');
+  recipeView.classList.remove('hidden');
+  singleFoodView.classList.remove('hidden');
+}
+
+function showCookPot() {
+  cookpotView.classList.remove('hidden');
+  recipeView.classList.add('hidden');
+}
